bcf6c6c9fed08d44e273550a9c1a4a16f6f52164
Haruxe <haruxe@proton.me>
2022-10-28 09:31:57 -0700

    add analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import ReactGA from "react-ga";
+
+ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const containter = document.getElementById("root");
 const root = ReactDOM.createRoot(containter);
